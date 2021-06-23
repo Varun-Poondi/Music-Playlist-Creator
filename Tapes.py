b8dde5c43c1672cd44e7feda398b6d29093cf5eb
varunpoondi <varunmukundan@gmail.com>
2021-06-22 19:10:29 -0500

    Updated variables and added simple user functions (still needs work)

--- Tapes.py
+++ Tapes.py
@@ -1,17 +1,26 @@
-from pytube import *
-import datetime
-
-
 class Tape:
-    def __init__(self, link, path):
-        def __seconds_to_minutes(seconds):
-            return str(datetime.timedelta(seconds=seconds))
-
-        self.video = YouTube(link)  # get video
-        self.thumbnail = self.video.thumbnail_url  # get jpg
-        self.artist = self.video.author
-        self.title = self.video.title
-        self.duration_seconds = self.video.length
-        self.duration_format = __seconds_to_minutes(self.video.length)
-        self.publish_date = self.video.publish_date
+    def __init__(self, path, author, name):
+        # What defines a tape
+        # All tapes are formatted in a particular way. Author - Title
         self.video_path = path
+        self.video_title = author + '-' + name
+        self.video_author = author
+        self.video_name = name
+
+    def __str__(self):
+        return 'Video Info: ' + \
+               '\nAuthor: ' + self.video_author + \
+               '\nName: ' + self.video_name + \
+               '\nPath: ' + self.video_path + '\n'
+
+    def change_name(self, name):
+        self.video_name = name
+        self.video_title = self.video_author + "-" + self.video_name
+
+    def change_author(self, author):
+        self.video_author = author
+        self.video_title = self.video_author + "-" + self.video_name
+
+    def change_path(self, path):
+        self.video_path = path
+
