54f3f8938e184ba049c38ac5e2520a31c921553e
varunpoondi <varunmukundan@gmail.com>
2021-06-22 19:09:34 -0500

    Cleaned code

--- PlayLists.py
+++ PlayLists.py
@@ -1,30 +1,35 @@
 from tkinter import *
-import tkinter.font as font
+from tkinter import font
+import Library as lib
+import ProjTools as pt
+import numpy as np
+
+global_playlist = []
 
 class PlayList:
-    def __init__(self, playlist_name, playlist):
-        self.playlist = playlist
+    def __init__(self, playlist_name, playlist_array):
         print(playlist_name)
-        for tabs in self.playlist:
-            print(tabs.title)
+
+        for i in range(len(playlist_array)):
+            print(str(i+1) + ':', playlist_array[i].__str__())
         return
 
 
 class PlayListFrame(Frame, PlayList):
     def __init__(self, root):
         Frame.__init__(self, root)
+        self.pL = pt.create_Frame_label(root, 'Your Playlists')
+        self.p_scrollBar = Scrollbar(root)
+        self.p_scrollBar.place(relx=0.1, rely=0.35, anchor=CENTER)
 
-        home_label = Label(root, text='My PlayLists', font=font.Font(family='Helvetica', size=20), bg='#EBDEF0', bd=3)
-        home_label.place(relx=0.5, rely=0.1, anchor=CENTER)
-
-        self.scrollbar = Scrollbar(root)
-        self.scrollbar.place(relx=0.1, rely=0.35, anchor=CENTER)
-
-        self.my_list = Listbox(root, yscrollcommand=self.scrollbar.set)
-        for line in range(100):
-            self.my_list.insert(END, "This is line number " + str(line))
+        self.my_list = Listbox(root, yscrollcommand=self.p_scrollBar.set)
+        self.my_list.insert(END, global_playlist)
 
         self.my_list.place(relx=0.15, rely=0.20)
-        self.scrollbar.config(command=self.my_list.yview)
+        self.p_scrollBar.config(command=self.my_list.yview)
+
+
 
         return
+
+
