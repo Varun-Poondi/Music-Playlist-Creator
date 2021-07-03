b6e3eda086df9b5e7900048dfba9054a62297596
varunpoondi <varunmukundan@gmail.com>
2021-07-03 15:13:07 -0500

    updated playlist DB and show updated list

--- PlaylistFrame.py
+++ PlaylistFrame.py
@@ -4,6 +4,7 @@ from tkmacosx import Button
 import ProjTools as pt
 import PlaylistDB as p_db
 
+
 class PlayListFrame(Frame):
     def __init__(self, root):
         Frame.__init__(self, root)
@@ -19,25 +20,22 @@ class PlayListFrame(Frame):
         self.lib_scroll()
 
         u_font = font.Font(family='Helvetica', size=16)
-        self.submit_button = Button(root, text='Refresh', font=u_font, bg='#A3E4D7', fg='#5F4B8B', borderless=1,
-                                    activebackground=('#AE0E36', '#D32E5E'), activeforeground='#E69A8D', padx=5,
-                                    command=self.__get_refresh)
-        self.submit_button.place(relx=0.99, rely=0.965, anchor=SE)
+        self.refresh_button = Button(root, text='Refresh', font=u_font, bg='#A3E4D7', fg='#5F4B8B', borderless=1,
+                                     activebackground=('#AE0E36', '#D32E5E'), activeforeground='#E69A8D', padx=5,
+                                     command=self.__get_refresh)
+        self.refresh_button.place(relx=0.99, rely=0.965, anchor=SE)
 
         self.select_button = Button(root, text='Select', font=u_font, bg='#A3E4D7', fg='#5F4B8B', borderless=1,
                                     activebackground=('#AE0E36', '#D32E5E'), activeforeground='#E69A8D', padx=5)
         self.select_button.place(relx=0.99, rely=0.27, anchor=SE)
         return
 
-
-
     def lib_scroll(self):
-        pass
-        # self.my_list.delete(0, END)
-        # cur = p_db.conn.cursor()
-        # for x in cur.execute("SELECT title FROM music"):
-        #     x = str(x)[2:-3]
-        #     self.my_list.insert(END, str(self.my_list.size() + 1) + ". " + x)
+        self.my_list.delete(0, END)
+        cur = p_db.conn.cursor()
+        for x in cur.execute("SELECT title FROM playlist"):
+            x = str(x)[2:-3]
+            self.my_list.insert(END, str(self.my_list.size() + 1) + ". " + x)
 
     def __get_refresh(self):
         self.lib_scroll()
